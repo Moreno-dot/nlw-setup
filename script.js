388a3d65a459514a5e57bdbdca37ac7300ba9a60
“Moreno-dot” <biielmoreno475@gmail.com>
2023-02-12 15:28:30 -0300

    Seleçao de habitos

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-const form = document.querySelector('#form-habits')
-const nlwSetup = new NLWSetup(form)
-const button = document.querySelector('header button')
+const form = document.querySelector("#form-habits");
+const select = document.querySelector("#habit-select");
+const nlwSetup = new NLWSetup(form);
+const button = document.querySelector("header button");
 
-button.addEventListener('click', add)
-form.addEventListener('change', save)
+button.addEventListener("click", add);
+form.addEventListener("change", save);
 
-function add(){
+function add() {
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5);
+  const dayExists = nlwSetup.dayExists(today);
 
-    const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
-    const dayExists = nlwSetup.dayExists(today)
+  if (dayExists) {
+    alert("Dia já incluso!");
+    return;
+  }
 
-    if(dayExists){
-        alert('Dia já incluso!')
-        return
-    }
-
-    alert('Registrado com sucesso! ✅')
-    nlwSetup.addDay(today)
+  alert("Registrado com sucesso! ✅");
+  nlwSetup.addDay(today, select.value);
 }
 
-function save(){
-    localStorage.setItem('NLWSetup-Habits', JSON.stringify(nlwSetup.data))
-    localStorage.getItem('NLWSetup-Habits')   
+function save() {
+  localStorage.setItem("NLWSetup-Habits", JSON.stringify(nlwSetup.data));
+  localStorage.getItem("NLWSetup-Habits");
 }
 
-const data = JSON.parse(localStorage.getItem('NLWSetup-Habits')) || {}
+const data = JSON.parse(localStorage.getItem("NLWSetup-Habits")) || {};
 
-nlwSetup.setData(data)
-nlwSetup.load()             
+nlwSetup.setData(data);
+nlwSetup.load();
